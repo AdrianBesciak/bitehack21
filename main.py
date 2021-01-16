28a80de13d595957280587c6060c050dba6a0bee
Adrian Besciak <adrian.besciak99@gmail.com>
2021-01-16 19:24:45 +0100

    added demo in main

--- main.py
+++ main.py
@@ -5,24 +5,35 @@ from guest import Guest
 
 
 def recognize_user(rfid_reader, guests):
+    print("start scanning")
     while True:
         uuid = rfid_reader.get_rfid_value()
+        print("received uuid: ", uuid)
         for guest in guests:
-            if uuid == guest.get_uuid():
+            if uuid[0:11] == guest.get_uuid():
                 return guest
+
+
+def follow_line(sensors, left_motor, right_motor):
+    print('Started line following')
+    while True:
+        line = sensors.get_line_sensors()
+        print(line + 'type: ' + str(type(line)))
+        left_motor.spin(50)
+        right_motor.spin(50)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    arduino = Arduino('ttyACM0')
+    arduino = Arduino('ttyUSB1')
 
-    guests = []
-    guests.add(Guest('Kamil', 'Ptak', 'AD:6F:55:D9'))
-
-    arrived_guest = recognize_user(rfid_reader=Arduino, guests=guests)
+    guests = [Guest('Kamil', 'Ptak', 'AD:6F:55:D9')]
+    print("scan rfid")
+    arrived_guest = recognize_user(rfid_reader=arduino, guests=guests)
 
     left_motor = MotorDriver(12, 20, 16)
     right_motor = MotorDriver(13, 5, 6)
+    follow_line(arduino, left_motor, right_motor)
     print('start running motor')
     while True:
         left_motor.spin(50)
