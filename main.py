1ba5023fdc37734b0669f724c7fa7a5939a2d651
Adrian Besciak <adrian.besciak99@gmail.com>
2021-01-16 16:26:03 +0100

    added motor driver sample in main

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from motor_driver import MotorDriver
+from time import sleep
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    left_motor = MotorDriver(12, 20, 16)
+    right_motor = MotorDriver(13, 5, 6)
+    print('start running motor')
+    while True:
+        left_motor.spin(50)
+        right_motor.spin(-100)
+        sleep(2)
+        left_motor.spin(100)
+        right_motor.spin(100)
+        sleep(1)
+        left_motor.spin(0)
+        right_motor.spin(100)
+        sleep(0.5)
+
+
+    print('stop running motor')
+
 
